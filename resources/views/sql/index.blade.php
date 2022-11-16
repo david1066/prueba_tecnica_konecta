263c19349561df4b465a7f2a7e4916f5c5c05f79
david1066 <davinchi1066@hotmail.com>
2022-11-16 16:26:34 -0500

    Copiado al portapapeles

--- resources/views/sql/index.blade.php
+++ resources/views/sql/index.blade.php
@@ -9,22 +9,18 @@
   <hr>
   <h2>Producto más vendido</h2>
   <pre>
-    <code>
-    SELECT productos.nombre, productos.referencia, MAX(ventas.cantidad) as cantidad
-    FROM prueba_tecnica_konecta.productos 
-    JOIN prueba_tecnica_konecta.ventas ON ventas.producto_id = productos.id 
-    GROUP BY productos.id
-    HAVING MAX(ventas.cantidad)
-    ORDER BY  cantidad desc limit 1
+    <code id="code1">
+    SELECT productos.nombre, productos.referencia, MAX(ventas.cantidad) as cantidad FROM prueba_tecnica_konecta.productos JOIN prueba_tecnica_konecta.ventas ON ventas.producto_id = productos.id GROUP BY productos.id HAVING MAX(ventas.cantidad) ORDER BY cantidad DESC LIMIT 1;
     </code>
+    <a id="BotonCopiar1" class="btn btn-info text-white">copiar</a>
   </pre>
   <hr>
   <h2>Producto con más stock</h2>
   <pre>
-    <code>
-    SELECT nombre, referencia, stock
-    FROM prueba_tecnica_konecta.productos ORDER BY stock DESC LIMIT 1;
+    <code id="code2">
+    SELECT nombre, referencia, stock FROM prueba_tecnica_konecta.productos ORDER BY stock DESC LIMIT 1;
     </code>
+    <a id="BotonCopiar2" class="btn btn-info text-white">copiar</a>
   </pre>
 </div>
 
@@ -32,3 +28,31 @@
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js" integrity="sha512-894YE6QWD5I59HgZOGReFYm4dnWc1Qt5NtvYSaNcOP+u1T9qYdvdihz0PPSiiqn/+/3e7Jo4EaG7TubfWGUrMQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+<script>
+
+
+document.getElementById("BotonCopiar1").onclick = function() {  
+  copiarAlPortapapeles('code1');
+};  
+document.getElementById("BotonCopiar2").onclick = function() {  
+  copiarAlPortapapeles('code2');
+};  
+function copiarAlPortapapeles(texto) {
+    // Obtener contenido del div oculto
+    let contenido = document.getElementById(texto).textContent;
+    // Crear input
+    let input = document.createElement('input');
+    // Asignar contenido
+    input.value = contenido;
+    // Agregar input a documento
+    document.body.appendChild(input);
+    // Seleccionar contenido
+    input.select();
+    // Copiar
+    document.execCommand('copy');
+    // Eliminar input
+    input.remove();
+    alert('copiado al portapapeles');
+}
+
+</script>
